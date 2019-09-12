0edfcccc3909da172aad8a86c0ab37d9f19da81f
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2019-09-12 12:56:15 +0200

    QQmlDelegateModel: implement support for reusing delegates
    Change-Id: I449c112fb55c8dfbe197ce0dc7cbb040f2f74ecb

--- src/qmlmodels/qqmldelegatemodel_p_p.h
+++ src/qmlmodels/qqmldelegatemodel_p_p.h
@@ -288,7 +288,7 @@ public:
 
     void requestMoreIfNecessary();
     QObject *object(Compositor::Group group, int index, QQmlIncubator::IncubationMode incubationMode);
-    QQmlDelegateModel::ReleaseFlags release(QObject *object);
+    QQmlDelegateModel::ReleaseFlags release(QObject *object, QQmlInstanceModel::ReusableFlag reusable = QQmlInstanceModel::NotReusable);
     QVariant variantValue(Compositor::Group group, int index, const QString &name);
     void emitCreatedPackage(QQDMIncubationTask *incubationTask, QQuickPackage *package);
     void emitInitPackage(QQDMIncubationTask *incubationTask, QQuickPackage *package);
@@ -300,8 +300,11 @@ public:
     void emitDestroyingItem(QObject *item) { Q_EMIT q_func()->destroyingItem(item); }
     void addCacheItem(QQmlDelegateModelItem *item, Compositor::iterator it);
     void removeCacheItem(QQmlDelegateModelItem *cacheItem);
-
     void updateFilterGroup();
+
+    void reuseItem(QQmlDelegateModelItem *item, int newModelIndex, int newGroups);
+    void insertIntoReusableItemsPool(QQmlDelegateModelItem *modelItem);
+    QQmlComponent *resolveDelegate(int index);
 
     void addGroups(Compositor::iterator from, int count, Compositor::Group group, int groupFlags);
     void removeGroups(Compositor::iterator from, int count, Compositor::Group group, int groupFlags);
@@ -347,6 +350,7 @@ public:
     QQmlDelegateModelGroupEmitterList m_pendingParts;
 
     QList<QQmlDelegateModelItem *> m_cache;
+    QQmlReuseableDelegateModelItemsPool m_reusableItemsPool;
     QList<QQDMIncubationTask *> m_finishedIncubating;
     QList<QByteArray> m_watchedRoles;
 
